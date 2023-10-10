6664b1c8a477bc334740b734371d0f8e98bfcbd8
herman-webdev <herman.web@outlook.com>
2023-10-10 23:35:14 +0300

    refactoring -v0.1

--- src/server/interfaces/user.ts
+++ src/server/interfaces/user.ts
@@ -2,6 +2,16 @@ export interface IUserId {
     id: string;
 }
 
+export interface IUserEmail {
+    id: string;
+    email: string;
+}
+
+export interface IPagination {
+    page: number;
+    pageSize: number;
+}
+
 export interface IUpdateData {
     firstName?: string;
     lastName?: string;
